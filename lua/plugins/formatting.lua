5ec965f363807e455a9b4537e93ca7b5e7b66a00
unixpup <dev@fault.wtf>
2025-03-13 10:33:33 -0300

    Add C/CXX formatting support

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -8,6 +8,8 @@ return {
 				javascript = { "prettier" },
 				css = { "prettier" },
 				rust = { "rustfmt" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
 			},
 			format_on_save = {
 				timeout_ms = 1000,
