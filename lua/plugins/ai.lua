71b7b03a2ededa02630ce37745f9b806adc7c6f5
unixpup <dev@fault.wtf>
2025-04-04 04:03:21 -0300

    CodeCompanion

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,24 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"github/copilot.vim", -- For Copilot support.
+	},
+
+	config = function()
+		require("codecompanion").setup({
+			strategies = {
+				chat = {
+					adapter = "copilot",
+				},
+				inline = {
+					adapter = "copilot",
+				},
+				cmd = {
+					adapter = "copilot",
+				},
+			},
+		})
+	end,
+}
